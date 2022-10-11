5648dd2dc7d7be7b3e13617be2b120f7808f371d
Marcos Oliveira <marcosvm@gmail.com>
2022-10-10 17:01:50 -0700

    add support for CDF serialization metrics

--- cmd/graphite_exporter/proxy/proxy.go
+++ cmd/graphite_exporter/proxy/proxy.go
@@ -5,9 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -37,6 +40,40 @@ type SSSS struct {
 
 func (s *SSSS) String() string {
 	return fmt.Sprintf("%s %s %s", s.Path, s.Value, s.Timestamp)
+}
+
+type CDF struct {
+	Values         []float64 `json:"values"`
+	DSTypes        []string  `json:"dstypes"`
+	DSNames        []string  `json:"dsnames"`
+	Time           float64   `json:"time"`
+	Interval       float64   `json:"interval"`
+	Host           string    `json:"host"`
+	Plugin         string    `json:"plugin"`
+	PluginInstance string    `json:"plugin_instance"`
+	Type           string    `json:"type"`
+	TypeInstance   string    `json:"type_instance"`
+	Meta           struct{}  `json:"-"`
+}
+
+func (c *CDF) String() string {
+	path := fmt.Sprintf("hosts.%s.%s", c.Host, c.Plugin)
+
+	if c.PluginInstance != "" {
+		path = fmt.Sprintf("%s-%s", path, c.PluginInstance)
+	}
+
+	path = fmt.Sprintf("%s.%s", path, c.Type)
+
+	if c.TypeInstance != "" {
+		typeInstance := strings.Replace(c.TypeInstance, ".", "_", -1)
+		path = fmt.Sprintf("%s-%s", path, typeInstance)
+	}
+
+	i, d := math.Modf(c.Time)
+	t := time.Unix(int64(i), int64(d*(1e9)))
+
+	return fmt.Sprintf("%s %f %d", path, c.Values[0], t.Unix())
 }
 
 func NewProxy(logger log.Logger, c chan string) *Proxy {
@@ -96,6 +133,17 @@ func (p *Proxy) Forward(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			p.input <- m.String()
+		case "SCDF":
+			var m []CDF
+			err := json.NewDecoder(bytes.NewReader(buf)).Decode(&m)
+			if err != nil {
+				level.Error(p.logger).Log("error decoding body", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			for _, metric := range m {
+				p.input <- metric.String()
+			}
 		default:
 			var m []SSSS
 			err := json.NewDecoder(bytes.NewReader(buf)).Decode(&m)
